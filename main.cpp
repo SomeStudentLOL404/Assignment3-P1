0d4f0c907ad7beddd422c96c6adec6ee05d0a573
SomeStudentLOL404 <44593734+SomeStudentLOL404@users.noreply.github.com>
2018-11-07 22:46:29 -0500

    Updated - Now nohardcoding so final version
    Old version was considered "hardcoding", this is how to actually do it

--- main.cpp
+++ main.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
-//#include "tokens.h" - We use later
-using namespace std;
+#include "tokens.h"
+#include "parse.h"
+#include "lex.h"
 
+
+using namespace std;
 int main(int argc, char* argv[])
 {
-        ifstream infile1;
-        for(int i = 1; i < argc; i++)
-        {
-            infile1.open(argv[i]);
-            if(argc > 2)
+    ifstream infile1;
+	istream *in = &cin;
+	int linenum = 0;
+    
+    for(int i = 1; i < argc; i++)
+    {       
+            string arg(argv[i]);
+            if(in != &cin)
             {
                 cout << "TOO MANY FILENAMES" << endl;
                 return -1;
             }
-            else if (infile1.is_open() == false)
-            {
-                cout << "COULD NOT OPEN " << argv[i] << endl;
-                return -1;
+            else {
+                infile1.open(arg);
+                if (infile1.is_open() == false)
+                {
+                    cout << "COULD NOT OPEN " << arg << endl;
+                    return -1;
+                }
+                in = &infile1;
             }
-            else if (infile1.peek() == EOF)
-            {
-                cout << "0: No statements in program" << endl;
-                return -1;
-            }
-        }
-    return 0; 
+     }
+    ParseTree *prog = Prog(in, &linenum);
+    if( prog == 0 )
+    {
+        return 0; // quit on error
+    }
+    
+    return 0;
 }
